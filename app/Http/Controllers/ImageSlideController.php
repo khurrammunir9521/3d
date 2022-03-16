55f481999e4ce24bae12e4d2601a2d2eb7a5772c
= <=>
2022-03-17 00:37:15 +0500

    responsive.css added

--- app/Http/Controllers/ImageSlideController.php
+++ app/Http/Controllers/ImageSlideController.php
@@ -53,7 +53,7 @@ class ImageSlideController extends Controller
             'heading'     => $request->heading,
             'sub_heading' => $request->sub_heading,
             'body_text'   => $request->body_text,
-            'images'      => $filePath,
+            'images2'      => $filePath,
         ]);
        if($sliders){
             return redirect()->route('image.index');
