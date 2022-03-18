cef5657bc0427b38d4399aeb41bfb3ac6594e5cd
= <=>
2022-03-18 16:17:27 +0500

    adasd

--- resources/views/pages/user/index/index.blade.php
+++ resources/views/pages/user/index/index.blade.php
@@ -54,9 +54,9 @@
 
     <div class="content-wrapper hide-on-load">
         <nav class="navbar navbar-expand-lg navbar-light">
-            <div class="container">
+            <div class="container-fluid">
                 <a class="navbar-brand" href="#">
-                    <img src="{{ asset('user/assets/icons/logo.svg') }}" alt="3dOrgans-logo" width="120" class="d-inline-block align-text-top">
+                    <img src="{{ asset('user/assets/icons/logo.svg') }}" alt="3dOrgans-logo" width="150" class="d-inline-block align-text-top">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
