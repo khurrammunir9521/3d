9354e704e1846c4f4bc2193e752f7a7eede01933
= <=>
2022-03-21 17:40:35 +0500

    latest Code

--- public/user/assets/js/scripts.js
+++ public/user/assets/js/scripts.js
@@ -1,19 +1,27 @@
 $(function () {
  
   // window.scrollTo(0, 0);
-
+if($(window).width() < 768) {
+  $('.banner-bg').removeClass('animate__slideInRight, animate__delay-2s, animate__slower');
+  $('.black-bg').removeClass('animate__slideInLeft');
+  $('.black-bg').addClass('animate__slideInUp');
+  $('.banner-bg').addClass('animate__slideInUp animate_slowest');
+}
   var i = 0;
   function move() {
-
+    $('.main-progress').css('z-index', '999');
+    $('.main-progress').removeClass('d-none');
     $('.hide-on-load').css('opacity', 0);
     if (i == 0) {
 
       i = 1;
       var elem = document.getElementById("barw");
       var widths = 1;
-      var id = setInterval(frame, 10);
+      var id = setInterval(frame, 25);
       function frame() {
         if (widths >= 100) {
+          $('.main-progress').css('z-index', '0');
+          $('.main-progress').addClass('d-none');
           clearInterval(id);
           i = 0;
           $(".main-progress").fadeTo(1200, 0);
@@ -695,4 +703,6 @@ $(function () {
       $('.modal.video').modal('hide');
     }
   })
+
+  
 })
