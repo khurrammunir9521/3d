65d1fc9d4015135313af4b603d9a86ab9cdb5a0c
asim7056 <asim@einnovention.co.uk>
2022-03-22 19:30:59 +0500

    dashboard

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -115,20 +115,16 @@
 
 
 <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-<!-- <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script> -->
     <!-- Scripts -->
 
     <script src="../assets/js/datatable/datatables/jquery.dataTables.min.js"></script>
     <script src="../assets/js/datatable/datatables/datatable.custom.js"></script>
     <script src="{{asset('user/assets/js/scripts.js')}}"></script>
-    <!-- <script>
+    <script>
         $(document).ready(function() {
             CKEDITOR.replace( 'ckeditor' );
-
-            $('#example').DataTable();
         });
-    </script> -->
+    </script>
     @stack('scripts')
 
 </body>
